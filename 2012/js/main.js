8210e8db85a2311d9ee53ef33b8bc1f3bdeddf93
root <nanhap@gmail.com>
2012-11-01 00:31:25 +0900

    tweet function update

--- 2012/js/main.js
+++ 2012/js/main.js
@@ -1,1 +1,27 @@
+$(function() {
+  $('#tweetFeed').jTweetsAnywhere({
+    searchParams: ['q=PlayNode', 'q=playnode'],
+    count: 10,
+    showTweetFeed: {
+      autorefresh: {
+        mode: 'trigger-insert',
+        interval: 10
+      },
+      showTimestamp: {
+        refreshInterval: 15
+      },
+      showTweetFeed: {
+        expandHovercards: true,
+        showSource: true
+      }
+    },
+    onDataRequestHandler: function(stats, options) {
+      if (stats.dataRequestCount < 11) {
+        return true;
+      } else {
+        stopAutorefresh(options);
+      }
+    }
+  });
+});
 
