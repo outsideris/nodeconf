7dd4f8da40f4af259981072b86b5dddbd81bba3a
root <nanhap@gmail.com>
2012-11-01 12:57:05 +0900

    footer, transition

--- 2012/js/main.js
+++ 2012/js/main.js
@@ -37,7 +37,7 @@ $(function() {
         $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
         if ($target.length) {
           var targetOffset = $target.offset().top;
-          $('html,body').animate({scrollTop: targetOffset}, 1000);
+          $('html,body').animate({scrollTop: targetOffset}, 300);
           return false;
         }
       }
