fbe5c30fd55d69649f0b91dd85feb361bd3e9e84
root <nanhap@gmail.com>
2012-11-10 20:40:58 +0900

    nanhapark 발표주제 약간 변경

--- 2012/js/main.js
+++ 2012/js/main.js
@@ -53,8 +53,8 @@ $(function() {
       "name": "박난하",
       "twitter": "nanhapark",
       "company" : "KTH",
-      "session": "Node.js In Production",
-      "contents": "Production 레벨에서 Node.js 를 사용함에 있어서 logging 부터 SSL까지 약 20가지의 필수적으로 코딩해야만 하는 핵심적인 부분에 대해서 알려드립니다. 이 부분을 알고 나시면 여러분은 고수 ps. 20가지중 realtime 부분에서 100 바이트도 안되는 socket.io tiny-prototyping 사용하여 간단한 장난감부터 Production Level (KTH에서 작업한 UCloud VM 자동생성, 자동설치) 에 적용하기까지의 단계를 설명합니다"
+      "session": "Node.js 본질",
+      "contents": "Production 레벨에서 Node.js 를 사용함에 있어서 logging 부터 Cluster까지 필수적으로 알아야 하는 핵심적인 부분에 대해서 알려드립니다. 이 부분을 알고 나시면 여러분은 고수 ps. 20가지중 realtime 부분에서 100 바이트도 안되는 socket.io tiny-prototyping 사용하여 간단한 장난감부터 Production Level (KTH에서 작업한 UCloud VM 자동생성, 자동설치) 에 적용하기까지의 단계를 설명합니다"
     },
     "b": {
       "name": "백정상",
